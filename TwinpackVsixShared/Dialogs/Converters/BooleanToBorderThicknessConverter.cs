9656ba67ff445add0725bc58532b08cc6ed86d98
Donkey <donkey@zeugwerk.at>
2023-09-15 13:09:38 +0200

    feat: default to install tab when any package is found

---
+++ TwinpackVsixShared/Dialogs/Converters/BooleanToBorderThicknessConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+using System.Globalization;
+
+namespace Twinpack.Dialogs
+{
+  public class BooleanToBorderThicknessConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      bool boolValue = (bool)value;
+      return boolValue ? new Thickness(0, 0, 0, 2) : new Thickness(0, 0, 0, 0);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
