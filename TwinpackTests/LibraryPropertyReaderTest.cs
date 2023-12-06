bbef4acfc0e162d92582e4a8dcdd6a260056d9a7
Donkey <donkey@zeugwerk.at>
2023-12-06 10:52:18 +0100

    fix: figured out how to properly decode the string table in a library file now we still need to know where the strings are actually used instead of blindly using them

--- TwinpackTests/LibraryPropertyReaderTest.cs
+++ TwinpackTests/LibraryPropertyReaderTest.cs
@@ -40,5 +40,24 @@ namespace TwinpackTests
             Assert.AreEqual("*", libraryInfo.Dependencies[3].Version);
             Assert.AreEqual("Tc3_Module", libraryInfo.Dependencies[3].Name);
         }
+
+        [TestMethod]
+        public void TestWithLongDescription()
+        {
+            var library = File.ReadAllBytes(@"assets\Untitled1.library");
+            var libraryInfo = Twinpack.LibraryPropertyReader.Read(library);
+
+            Assert.IsNotNull(library);
+            Assert.AreEqual("XY", libraryInfo.Company);
+            Assert.AreEqual("XY", libraryInfo.Title);
+            Assert.AreEqual("1.0.0.0", libraryInfo.Version);
+            Assert.AreEqual(@"123456790abcdefghijklmnopqrstuvwxyz 123456790abcdefghijklmnopqrstuvwxyz 123456790abcdefghijklmnopqrstuvwxyz 123456790abcdefghijklmnopqrstuvwxyz 123456790abcdefghijklmnopqrstuvwxyz 123456790abcdefghijklmnopqrstuvwxyz 123456790abcdefghijklmnopqrstuvwxyz 123456790abcdefghijklmnopqrstuvwxyz 123456790abcdefghijklmnopqrstuvwxyz 123456790abcdefghijklmnopqrstuvwxyz 123456790abcdefghijklmnopqrstuvwxyz 123456790abcdefghijklmnopqrstuvwxyz 123456790abcdefghijklmnopqrstuvwxyz ", libraryInfo.Description);
+            Assert.AreEqual("", libraryInfo.Author);
+            Assert.AreEqual("XY", libraryInfo.DefaultNamespace);
+            Assert.AreEqual(1, libraryInfo.Dependencies.Count);
+            Assert.AreEqual("Beckhoff Automation GmbH", libraryInfo.Dependencies[3].DistributorName);
+            Assert.AreEqual("*", libraryInfo.Dependencies[3].Version);
+            Assert.AreEqual("Tc3_Module", libraryInfo.Dependencies[3].Name);
+        }
     }
 }
