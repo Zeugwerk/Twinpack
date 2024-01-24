41187d7c89ca307e00213e93326e677590125e3f
Donkey <iadonkey@zeugwerk.at>
2024-01-24 14:11:35 +0100

    fix: when creating a config from a sln file, projects are ignored

--- TwinpackShared/Models/Config.cs
+++ TwinpackShared/Models/Config.cs
@@ -1,12 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Text.RegularExpressions;
-using System.Xml.Linq;
 
 namespace Twinpack.Models
 {
