01c674c33b9b1a0b57dd933ea9be87f596bafd45
donkey <84121166+iadonkey@users.noreply.github.com>
2024-04-30 09:07:17 +0200

    feat: hide close button in package server configuration

--- TwinpackVsixShared/Dialogs/PackagingServerDialog.cs
+++ TwinpackVsixShared/Dialogs/PackagingServerDialog.cs
@@ -16,6 +16,13 @@ namespace Twinpack.Dialogs
 {
     public partial class PackagingServerDialog : Window, INotifyPropertyChanged
     {
+        private const int GWL_STYLE = -16;
+        private const int WS_SYSMENU = 0x80000;
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+    
         public event PropertyChangedEventHandler PropertyChanged;
         private static readonly NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
         string _urlMem;
@@ -40,6 +47,10 @@ namespace Twinpack.Dialogs
         private async void Dialog_Loaded(object sender, RoutedEventArgs e)
         {
             await Microsoft.VisualStudio.Shell.ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            // hide window close button
+            var hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+            SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_SYSMENU);
         }
 
         private async void AddButton_Click(object sender, RoutedEventArgs e)
