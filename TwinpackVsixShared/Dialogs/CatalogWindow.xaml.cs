88bcf0def1ce4b846ff876d0627697dc9416ead4
Donkey <donkey@zeugwerk.at>
2025-01-05 19:36:13 +0100

    fix: package version loading indicator stays busy if package, referenced in a config.json file, is not actually available

--- TwinpackVsixShared/Dialogs/CatalogWindow.xaml.cs
+++ TwinpackVsixShared/Dialogs/CatalogWindow.xaml.cs
@@ -1074,6 +1074,7 @@ namespace Twinpack.Dialogs
             finally
             {
                 IsPackageLoading = false;
+                IsPackageVersionLoading = false;
             }
         }
 
