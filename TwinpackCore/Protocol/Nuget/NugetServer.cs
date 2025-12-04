6b8d5656eaf8a870a5b6ced3eae22f3e03164ec7
Donkey <donkey@zeugwerk.at>
2025-12-04 22:51:09 +0100

    fix: nullpointers

--- TwinpackCore/Protocol/Nuget/NugetServer.cs
+++ TwinpackCore/Protocol/Nuget/NugetServer.cs
@@ -663,11 +663,6 @@ namespace Twinpack.Protocol
             catch { }
         }
 
-        protected virtual string EvaluateVersion(NuGetVersion version)
-        {
-            return version.OriginalVersion.ToString();
-        }
-
 #pragma warning disable CS1998 // Bei der asynchronen Methode fehlen "await"-Operatoren. Die Methode wird synchron ausgeführt.
         public async Task LogoutAsync()
 #pragma warning restore CS1998 // Bei der asynchronen Methode fehlen "await"-Operatoren. Die Methode wird synchron ausgeführt.
@@ -730,6 +725,11 @@ namespace Twinpack.Protocol
                 return package.Title;
 
             return package.Identity.Id;
+        }
+
+        protected virtual string EvaluateVersion(NuGetVersion version)
+        {
+            return version?.OriginalVersion?.ToString();
         }
 
         protected virtual int EvaluateCompiled(string tags)
