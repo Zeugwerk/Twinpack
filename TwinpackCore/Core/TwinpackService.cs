cf94e25ac2dc90c51547b7f71418d753ee158207
Stefan Besler <stefan.besler@zeugwerk.at>
2026-05-14 23:44:05 +0200

    fix: update all can install stale packages

--- TwinpackCore/Core/TwinpackService.cs
+++ TwinpackCore/Core/TwinpackService.cs
@@ -360,7 +360,7 @@ namespace Twinpack.Core
             }
         }
 
-        public async Task<IEnumerable<PackageItem>> RetrieveUsedPackagesAsync(string searchTerm = null, bool includeMetadata = false, List<string> excludedPackages = null, CancellationToken token = default)
+        public async Task<IEnumerable<PackageItem>> RetrieveUsedPackagesAsync(string searchTerm = null, bool includeMetadata = false, List<string> excludedPackages = null, CancellationToken token = default, bool refreshUsedPackageCache = false)
         {
             if (_config.Modules?.Any() == true)
                 throw new NotSupportedException("Modules are not supported");
@@ -368,6 +368,9 @@ namespace Twinpack.Core
             try
             {
                 await _usedPackagesMutex.WaitAsync();
+
+                if (refreshUsedPackageCache)
+                    _usedPackagesCache.Clear();
 
                 foreach (var project in _config.Projects.Where(x => x.Name == _projectName || _projectName == null))
                 {
@@ -487,7 +490,7 @@ namespace Twinpack.Core
 
         public async System.Threading.Tasks.Task<List<PackageItem>> UpdatePackagesAsync(UpdatePackageFilters filters = default, UpdatePackageOptions options = default, CancellationToken cancellationToken = default)
         {
-            var usedPackages = await RetrieveUsedPackagesAsync();
+            var usedPackages = await RetrieveUsedPackagesAsync(token: cancellationToken, refreshUsedPackageCache: true);
             List<PackageItem> packages;
             if (filters?.Packages != null || filters?.Frameworks != null)
             {
@@ -694,6 +697,9 @@ namespace Twinpack.Core
             packageItem.PackageVersion = resolvedPackage.PackageVersion;
             packageItem.PackageServer = resolvedPackage.PackageServer;
             packageItem.Dependencies = resolvedPackage.Dependencies;
+            packageItem.Update = resolvedPackage.Update;
+            if (resolvedPackage.Used != null)
+                packageItem.Used = resolvedPackage.Used;
         }
 
         public async Task<List<PackageItem>> AffectedPackagesAsync(List<PackageItem> packages, bool includeDependencies = true, CancellationToken cancellationToken = default)
